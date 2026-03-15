a5d81818d01df875d900a3ccd0229ccefe78fd71
Supriyo Sarkar <supriyoss18@gmail.com>
2026-03-16 02:59:06 +0530

    terraform bug fix

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
-  default     = "webapp-rg"
+  default     = "techish-webapp-rg"
 }
 
 variable "location" {
