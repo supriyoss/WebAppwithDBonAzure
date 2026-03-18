48c65402d20114b061fcc6dbb747315874fe5924
Supriyo Sarkar <supriyoss18@gmail.com>
2026-03-19 03:30:12 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -13,15 +13,14 @@ provider "azurerm" {
   use_msi = true
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
 }
 
 resource "azurerm_postgresql_flexible_server" "postgres" {
   name                = var.postgres_server_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   sku_name            = "B_Standard_B1ms"
   storage_mb          = 32768
   backup_retention_days = 7
